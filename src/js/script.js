40831c32b80d73ab1af563ef3e00b45692b3eb24
Maria Fernanda <90789503+MaferRs@users.noreply.github.com>
2024-02-01 23:19:44 -0300

    :hammer: add animation

--- src/js/script.js
+++ src/js/script.js
@@ -89,28 +89,39 @@ swiper.update();
 
 // seção de perguntas e respostas.
 
-var buttons = document.querySelectorAll('.button-doubts')
-var respostas = document.querySelectorAll('#resposta')
+function faq() {
 
-respostas[0].style.display = "block";
+    var buttons = document.querySelectorAll('.button-doubts')
+    var respostas = document.querySelectorAll('#resposta')
 
-buttons.forEach(function (button) {
-    button.addEventListener('click', function () {
-        // Esconde todas as respostas
-        respostas.forEach(function (resposta) {
-            resposta.style.display = "none";
+    respostas[0].style.display = "block";
+
+    buttons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            // Esconde todas as respostas
+            respostas.forEach(function (resposta) {
+                resposta.style.display = "none";
+            });
+
+            // Mostra a resposta do botão clicado
+            var resposta = this.parentNode.nextElementSibling;
+            if (resposta.style.display == 'none') {
+                resposta.style.display = "block";
+
+            } else {
+                resposta.style.display = "none";
+            }
+            console.log(resposta)
         });
-
-        // Mostra a resposta do botão clicado
-        var resposta = this.parentNode.nextElementSibling;
-        if (resposta.style.display == 'none') {
-            resposta.style.display = "block";
-        } else {
-            resposta.style.display = "none";
-        }
-        console.log(resposta)
     });
-});
+
+}
+
+faq();
 
 
+AOS.init({
+    duration: 2000,
+    once: true,
 
+})
